fc087115bf05bfdc3dfe1d4668a14a83fafd6809
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-26 14:20:30 +0600

    Adding timer mechanism.

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5)
+    count_down(SHORT_BREAK_MIN * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
 
-    count_min = SHORT_BREAK_MIN / 60
-    count_sec = SHORT_BREAK_MIN % 60
-    canvas.itemconfig(timer, text=count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
 
