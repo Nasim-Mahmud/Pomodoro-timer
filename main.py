f51a21062cb0a47dba67ad00077d4994638a779b
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-26 14:08:10 +0600

    Adding timer mechanism.

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
@@ -19,12 +20,12 @@ def count_down(count):
     canvas.itemconfig(timer, text=count)
     if count > 0:
         window.after(1000, count_down, count - 1)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-
-
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
@@ -49,14 +50,5 @@ reset_button.grid(row=2, column=2)
 check_mark = Label()
 check_mark.config(text="✔", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 16, "bold"))
 check_mark.grid(row=3, column=1)
-
-
-
-
-
-
-
-
-
 
 window.mainloop()
