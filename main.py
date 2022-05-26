4dd7347382e56719cee43e00e2df4494fb47d777
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-26 13:41:24 +0600

    Adding attributes

--- main.py
+++ main.py
@@ -37,5 +37,14 @@ start_button = Button(text="Start", font=(FONT_NAME, 10, "bold"))
 start_button.config(width=5, height=1)
 start_button.grid(row=2, column=0)
 
+reset_button = Button(text="Reset", font=(FONT_NAME, 10, "bold"))
+reset_button.config(width=5, height=1)
+reset_button.grid(row=2, column=2)
+
+check_mark = Label()
+check_mark.config(text=" ")
+
+
+
 
 window.mainloop()
