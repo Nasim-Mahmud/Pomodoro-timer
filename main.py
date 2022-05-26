13db0b2ed96e790ca26e495fea2c85a0aaf7a7bc
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-26 22:17:48 +0600

    Setting timer mechanism and adding break time reps.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
 
+
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
@@ -65,7 +66,6 @@ label = Label()
 label.config(text="Timer", font=(FONT_NAME, 35, "bold"), bg=YELLOW, fg=GREEN)
 label.grid(row=0, column=1)
 
-
 # -----------------------------BUTTONS-------------------------------- #
 start_button = Button(text="Start", font=(FONT_NAME, 10, "bold"))
 start_button.config(width=5, height=1, command=start_timer)
@@ -74,7 +74,6 @@ start_button.grid(row=2, column=0)
 reset_button = Button(text="Reset", font=(FONT_NAME, 10, "bold"))
 reset_button.config(width=5, height=1)
 reset_button.grid(row=2, column=2)
-
 
 # -----------------------------CHECK MARKS-------------------------------- #
 check_mark = Label()
