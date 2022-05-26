d3e8c894c8a74878ed8346f9244dce2a5ead2592
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-26 13:53:41 +0600

    Adding attributes

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
 canvas.grid(row=1, column=1)
 
 label = Label()
@@ -42,5 +42,14 @@ reset_button.grid(row=2, column=2)
 check_mark = Label()
 check_mark.config(text="✔", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 16, "bold"))
 check_mark.grid(row=3, column=1)
+
+
+
+
+
+
+
+
+
 
 window.mainloop()
